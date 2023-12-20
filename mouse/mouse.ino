c7fb0735cc43e4c94ad98d62e171e64f22006627
xPrimoria <153913362+xPrimoria@users.noreply.github.com>
2023-12-20 15:42:38 +0300

    Update mouse.ino

--- mouse/mouse.ino
+++ mouse/mouse.ino
@@ -8,30 +8,24 @@ void setup() {
 void loop() {
   if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
-    processCommand(command);
-  }
-}
+    if (command == "CLICK") {
+      Mouse.click();
+    } else {
+      int commaIndex = command.indexOf(',');
+      if (commaIndex != -1) {
+        int x = command.substring(0, commaIndex).toInt();
+        int y = command.substring(commaIndex + 1).toInt();
 
-void processCommand(String command) {
-  if (command.startsWith("Temperature:")) {
-    int pressureIndex = command.indexOf("Pressure:");
-    int lightIndex = command.indexOf("Ambient Light:");
-    
-    if (pressureIndex != -1 && lightIndex != -1) {
-      int y = command.substring(pressureIndex + 9, command.indexOf(" hPa", pressureIndex)).toInt() - 987;
-      int x = command.substring(lightIndex + 14, command.indexOf(" Lux", lightIndex)).toInt() - 5323;
+        while (x != 0 || y != 0) {
+          int moveX = constrain(x, -127, 127);
+          int moveY = constrain(y, -127, 127);
 
-      while (x != 0 || y != 0) {
-        int moveX = constrain(x, -128, 127);
-        int moveY = constrain(y, -128, 127);
+          Mouse.move(moveX, moveY);
 
-        Mouse.move(moveX, moveY);
-
-        x -= moveX;
-        y -= moveY;
+          x -= moveX;
+          y -= moveY;
+        }
       }
     }
-  } else if (command == "Send_Temperature_Report") {
-    Mouse.click();
   }
 }
