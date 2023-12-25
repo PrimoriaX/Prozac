c6fa9a5affe5cb0e8bf82db924f916f770a99906
Primoria <153913362+PrimoriaX@users.noreply.github.com>
2023-12-25 14:46:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,9 +37,7 @@ def prints():
                                         ▐█▪·•▐█•█▌▐█▌.▐▌█▌▪▄█▀▐█ ▪▐▌▐███▌
                                         .▀   .▀  ▀ ▀█▄▀▪·▀▀▀ • ▀  ▀ ·▀▀▀
     """
-
     ascii_art = ascii_art.replace("@", f"{Fore.GREEN}@{Style.RESET_ALL}").replace(".", f"{Fore.WHITE}.{Style.RESET_ALL}")
-
     support_discord = f"{Fore.LIGHTBLACK_EX}\t\t\t  [{Fore.GREEN}x{Fore.LIGHTBLACK_EX}]{Style.RESET_ALL} Join our support discord server: {Fore.GREEN}discord.gg/bsNKqvxvE2   {Style.RESET_ALL}"
     
     print(ascii_art)
